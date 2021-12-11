00c8208c8718828994399b15d9f43f1d37b21379
StevePro7 <steveprozune@hotmail.com>
2021-12-11 13:25:37 +0000

    no message

--- 00/dev/build.bat
+++ 00/dev/build.bat
@@ -10,6 +10,7 @@ set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 :: Compile
 cd devkit
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
 cd ..
 
 cd engine
--- 00/dev/engine/screen_manager.c
+++ 00/dev/engine/screen_manager.c
@@ -1,6 +1,8 @@
 #include "screen_manager.h"
 #include "font_manager.h"
+#include "input_manager.h"
 #include "sprite_manager.h"
+#include "../devkit/_snd_manager.h"
 
 void engine_screen_manager_init()
 {
@@ -9,5 +11,13 @@ void engine_screen_manager_init()
 
 void engine_screen_manager_update()
 {
+	unsigned char input;
+	input = engine_input_manager_hold_fire1();
+	if( input )
+	{
+		devkit_PSGStop();
+		return;
+	}
+
 	engine_sprite_manager_draw( 120, 88, 256 );
 }
