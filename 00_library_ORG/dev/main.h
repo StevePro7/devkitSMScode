8e8638c4a8d434ad84fe26f443dd4bef82227615
StevePro7 <steveprozune@hotmail.com>
2021-12-23 18:23:46 +0000

    no message

--- 00_library_ORG/dev/main.h
+++ 00_library_ORG/dev/main.h
@@ -7,10 +7,4 @@
 // LIBRARIES.
 #include "devkit/_sms_manager.h"
 
-#include "engine/asm_manager.h"
-#include "engine/content_manager.h"
-#include "engine/font_manager.h"
-#include "engine/input_manager.h"
-#include "engine/screen_manager.h"
-
 #endif//__MAIN__
