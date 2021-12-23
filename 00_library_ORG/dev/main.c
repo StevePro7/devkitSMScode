a2040e6212b272064bd4aeb0c3e9fdb6eed347e8
StevePro7 <steveprozune@hotmail.com>
2021-12-23 19:57:10 +0000

    no message

--- 00_library_ORG/dev/main.c
+++ 00_library_ORG/dev/main.c
@@ -7,23 +7,23 @@ void main( void )
 	//engine_asm_manager_clear_VRAM();
 	
 	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
-	devkit_SMS_useFirstHalfTilesforSprites_False();
-	//devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
+	devkit_SMS_useFirstHalfTilesforSprites( false );
+	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
 	//engine_content_manager_load_tiles();
 	//engine_screen_manager_init();
 
-	devkit_SMS_setSpritePaletteColor( 0, devkit_RGB( 0, 0, 3 ) );
+	devkit_SMS_setSpritePaletteColor( 0, devkit_RGB( 3, 3, 0 ) );
 
 	devkit_SMS_displayOn();
 	for( ;; )
 	{
-		//devkit_SMS_initSprites();
+		devkit_SMS_initSprites();
 		//engine_input_manager_update();
 		//engine_screen_manager_update();
 
-		//devkit_SMS_finalizeSprites();
+		devkit_SMS_finalizeSprites();
 		devkit_SMS_waitForVBlank();
-		//devkit_SMS_copySpritestoSAT();
+		devkit_SMS_copySpritestoSAT();
 	}
 }
