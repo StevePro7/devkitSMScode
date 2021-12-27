68816c78ed373238cda8bbd6451d9e6b5d0f6ef9
StevePro7 <steveprozune@hotmail.com>
2021-12-27 13:52:29 +0000

    no message

--- 00_library_ORG/dev/engine/font_manager.adb
+++ 00_library_ORG/dev/engine/font_manager.adb
@@ -19,12 +19,12 @@ S:Lfont_manager.engine_font_manager_draw_data$y$1$74({1}SC:U),B,1,7
 S:Lfont_manager.engine_font_manager_draw_data$pnt$1$75({2}DG,SC:U),R,0,0,[]
 S:Lfont_manager.engine_font_manager_draw_data$idx$1$75({1}SC:U),B,1,-6
 S:Lfont_manager.engine_font_manager_draw_data$tile$1$75({1}SC:S),R,0,0,[c]
-S:Lfont_manager.engine_font_manager_draw_data$quotient$1$75({2}SI:U),B,1,-4
+S:Lfont_manager.engine_font_manager_draw_data$quotient$1$75({2}SI:U),B,1,-3
 S:Lfont_manager.engine_font_manager_draw_data$remainder$1$75({1}SC:U),R,0,0,[e]
 S:Lfont_manager.engine_font_manager_draw_data$hold$1$75({4}DA4d,SC:U),B,1,-10
-S:Lfont_manager.engine_font_manager_draw_data$sloc0$1$0({2}DD,SC:U),B,1,-2
-S:Lfont_manager.engine_font_manager_draw_data$sloc1$1$0({1}SC:U),B,1,-5
-S:Lfont_manager.engine_font_manager_draw_data$sloc2$1$0({2}SI:U),B,1,-4
+S:Lfont_manager.engine_font_manager_draw_data$sloc0$1$0({2}DD,SC:U),B,1,-5
+S:Lfont_manager.engine_font_manager_draw_data$sloc1$1$0({1}SC:U),B,1,-1
+S:Lfont_manager.engine_font_manager_draw_data$sloc2$1$0({2}SI:U),B,1,-3
 F:G$engine_font_manager_draw_zero$0$0({2}DF,SV:S),Z,0,4,0,0,0
 S:Lfont_manager.engine_font_manager_draw_zero$data$1$79({2}SI:U),B,1,4
 S:Lfont_manager.engine_font_manager_draw_zero$x$1$79({1}SC:U),B,1,6
